176f438b543894b2aa6d6e792d58e90dd5aebcf9
Ubuntu <ubuntu@ip-172-31-93-49.ec2.internal>
2019-09-13 17:08:33 +0000

    commit adde rectangle program

---
+++ rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Patrick Gemmell
+# Created on: September 12 2019
+# This program runs Hello World on pyhton
+
+
+def main():
+    print("The dimesnsions of the rectangle are:")
+    print("5mm x 3mm")
+    print("")
+    print("")
+    print("The area is {}".format(3*5))
+    print("The perimeter is {}".format(3*5))
+
+
+if __name__ == "__main__":
+    main()
